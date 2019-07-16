047811f514ddbe0d2874711f6627ff9c37bc1e26
Soumahoro Karim DCG <sylvestregaye@gmail.com>
2019-07-16 17:40:39 +0000

    initial commit

---
+++ src/main/webapp/resources/assets/js/question/questions.js
@@ -0,0 +1,49 @@
+$(window).load(function(){
+
+    //initialize editable datatable
+
+    var context = $('.brand').attr("href");
+    var table2 = $('#editable-usage');
+
+    var oTable = $('#editable-usage').DataTable({
+        "aoColumnDefs": [
+            { 'bSortable': false, 'aTargets': [ "no-sort" ] }
+        ]
+    });
+
+
+    table2.on('click', '.delete', function (e) {
+        e.preventDefault();
+
+        var id = $(this).attr("id");
+
+        if (confirm("êtes-vous sûr de la suppression?") == false) {
+            return;
+        }
+
+        $.ajax({
+            url: context + 'questions/delete/' + id,
+            method: 'GET',
+            data: null,
+            success: function (data) {
+                if (data.msg === 'ok') {
+                    alert("Supprimé!");
+                }
+                else {
+                    alert("Suppression imposibble!");
+                }
+
+                location.href = context + 'questions';
+            },
+            error: function () {
+                alert("Suppression imposibble!");
+                location.href = context + 'questions';
+            }
+        });
+
+    });
+
+
+    //*initialize editable datatable
+
+});
